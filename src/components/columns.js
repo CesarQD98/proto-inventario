20a2795168a8a1a3cde8c888f6ed484d6d93ae28
Rico98 <asd.cqd98@gmail.com>
2022-11-03 00:08:24 -0500

    Added files to components folder
    Added MOCK_DATA.json for running tests, added column.js to manage the columns showed in the website.

---
+++ src/components/columns.js
@@ -0,0 +1,38 @@
+export const COLUMNS = [
+    {
+        Header: 'Id',
+        accessor: 'id'
+    },
+    {
+        Header: 'Equipo',
+        accessor: 'equipment'
+    },
+    {
+        Header: 'Código',
+        accessor: 'code'
+    },
+    {
+        Header: 'Marca',
+        accessor: 'brand'
+    },
+    {
+        Header: 'Tiempo de entrega',
+        accessor: 'delivery_time'
+    },
+    {
+        Header: 'Lugar de entrega',
+        accessor: 'delivery_place'
+    },
+    {
+        Header: 'Proveedor',
+        accessor: 'supplier'
+    },
+    {
+        Header: 'Procedencia',
+        accessor: 'currency'
+    },
+    {
+        Header: 'Costo UNIT',
+        accessor: 'amount'
+    }
+]
